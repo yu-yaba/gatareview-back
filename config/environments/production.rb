fc9d1a42faef07276af754c4dc2b1b94d620d64a
yu-yaba <kawai.yuya3401@gmail.com>
2023-09-16 21:13:47 +0900

    productionのアセットパイプラインを無効

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  # config.assets.compile = true
 
   config.force_ssl = true
 
