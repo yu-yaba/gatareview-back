822098c9e04325986bcdf68c7e8e4bd4503db3b2
yu-yaba <kawai.yuya3401@gmail.com>
2023-09-16 17:45:06 +0900

    Fix heroku.yml and Dockerfile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,7 +38,6 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
@@ -97,7 +96,6 @@ Rails.application.configure do
 
   config.active_storage.service = :amazon
 
-  Rails.application.routes.default_url_options[:host] = 'niigata-univ-lecture-review-5a3fbbeb0fc7.herokuapp.com'
 
 
 end
