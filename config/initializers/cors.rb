92271dc4f66e141f1eccf900aa6fd8657204e592
yu-yaba <kawai.yuya3401@gmail.com>
2023-09-17 23:23:34 +0900

    corsの修正

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,8 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:8080"
-
-    resource "*",
+    origins 'localhost:8080', '127.0.0.1:8080', 'https://gatareview-front.vercel.app', 'https://www.gatareview.com'
+    resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
