eaeefb78fa3f0559bc610e25475c29429d2ab463
yu-yaba <kawai.yuya3401@gmail.com>
2024-01-20 15:51:40 +0900

    rubocop -A

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,3 +1,9 @@
-class Api::V1::ApiController < ActionController::Base
-  protect_from_forgery with: :null_session
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class ApiController < ActionController::Base
+      protect_from_forgery with: :null_session
+    end
+  end
 end
