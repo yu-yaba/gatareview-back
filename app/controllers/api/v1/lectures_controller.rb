fee596a7253dad3e7d8c975a138b324430b64de4
yu-yaba <kawai.yuya3401@gmail.com>
2024-01-24 15:33:08 +0900

    コントローラーをリファクタリング

--- app/controllers/api/v1/lectures_controller.rb
+++ app/controllers/api/v1/lectures_controller.rb
@@ -4,38 +4,11 @@ module Api
   module V1
     class LecturesController < ApplicationController
       def index
-        query_conditions = {}
+        @lectures = Lecture.search(params[:faculty], params[:searchWord])
 
-        query_conditions[:faculty] = params[:faculty] if params[:faculty].present?
+        render json: { error: 'Either faculty or searchWord must be specified.' }, status: 400 if @lectures.empty?
 
-        query_conditions[:title] = params[:searchWord] if params[:searchWord].present?
-
-        if query_conditions.empty?
-          render json: { error: 'Either faculty or searchWord must be specified.' }, status: 400
-          return
-        end
-
-        @lectures = Lecture.includes(:reviews)
-                           .where('faculty LIKE :faculty OR title LIKE :searchWord', faculty: "%#{query_conditions[:faculty]}%", searchWord: "%#{query_conditions[:title]}%")
-
-        lecture_ids = @lectures.pluck(:id)
-        avg_ratings = Review.where(lecture_id: lecture_ids).group(:lecture_id).average(:rating)
-
-        @lectures_json = @lectures.map do |lecture|
-          lecture_attributes = lecture.attributes
-          avg_rating = avg_ratings[lecture.id.to_s] || 0
-          lecture_attributes[:avg_rating] = avg_rating.round(1)
-          lecture_attributes[:reviews] = lecture.reviews.map do |review|
-            {
-              id: review.id,
-              content: review.content,
-              rating: review.rating,
-              created_at: review.created_at,
-              updated_at: review.updated_at
-            }
-          end
-          lecture_attributes
-        end
+        @lectures_json = Lecture.as_json_reviews(@lectures)
 
         render json: @lectures_json
       end
