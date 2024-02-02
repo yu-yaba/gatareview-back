81fed90624d0e0404d4f77a2b4a729aa67098bea
yu-yaba <kawai.yuya3401@gmail.com>
2024-02-02 17:06:14 +0900

    rubocop -A

--- app/controllers/api/v1/reviews_controller.rb
+++ app/controllers/api/v1/reviews_controller.rb
@@ -1,34 +1,40 @@
-class Api::V1::ReviewsController < ApplicationController
-  before_action :set_lecture, except: [:total]
+# frozen_string_literal: true
 
-  def index
-    reviews = @lecture.reviews
-    render json: reviews
-  end
+module Api
+  module V1
+    class ReviewsController < ApplicationController
+      before_action :set_lecture, except: [:total]
 
-  def create
-    @review = @lecture.reviews.new(review_params)
+      def index
+        reviews = @lecture.reviews
+        render json: reviews
+      end
 
-    if @review.save
-      render json: @review, status: :created
-    else
-      render json: @review.errors, status: :unprocessable_entity
+      def create
+        @review = @lecture.reviews.new(review_params)
+
+        if @review.save
+          render json: @review, status: :created
+        else
+          render json: @review.errors, status: :unprocessable_entity
+        end
+      end
+
+      def total
+        total_reviews = Review.count
+        render json: { count: total_reviews }
+      end
+
+      private
+
+      def set_lecture
+        @lecture = Lecture.find(params[:lecture_id])
+      end
+
+      def review_params
+        params.require(:review).permit(:rating, :content, :period_year, :period_term, :textbook, :attendance,
+                                       :grading_type, :content_difficulty, :content_quality)
+      end
     end
-  end
-
-  def total
-    total_reviews = Review.count
-    render json: { count: total_reviews }
-  end
-
-  private
-
-  def set_lecture
-    @lecture = Lecture.find(params[:lecture_id])
-  end
-
-  def review_params
-    params.require(:review).permit(:rating, :content, :period_year, :period_term, :textbook, :attendance,
-                                   :grading_type, :content_difficulty, :content_quality)
   end
 end
--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Lecture < ApplicationRecord
   has_many :reviews
   has_many_attached :images
